2e6110c772fa22b02e7809e4cdafd9b5ab8d4d52
adityash97 <adityash212@gmail.com>
2025-07-29 10:58:09 +0530

    auth and protected routes

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+export default function login (){
+    return <>
+    <h1>This is a login page</h1>
+    </>
+}
---
+++ frontend/src/app/(auth)/logout/page.tsx
@@ -0,0 +1,5 @@
+export default function logout (){
+    return <>
+    <h1>This is a logout page</h1>
+    </>
+}
